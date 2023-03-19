1a750ef859bf8689588c364a659ccd23c9748e14
Bishwas Aryal <aryalbishwas123@gmail.com>
2023-03-19 21:46:09 +0545

    everything except minmax()

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -60,34 +60,42 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    countx=0
-    county=0
-    countnone=0
+    prev = board[0][0]
+    prev2 = board[0][0]
     for i in range(3):
-        for j in range(3):
-            if board[i][j] == X:
-                countx+=1
-            elif board[i][j] == O:
-                county+=1
-            else:
-                countnone+=1
-
-
-    raise NotImplementedError
+        j=0
+        if board[i][j]==board[i][j+1]==board[i][j+2]:
+            return board[i][j]
+        if board[j][i]==board[j+1][i]==board[j+2][i]:
+            return board[j][i]
+    if board[0][0]== board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2]== board[1][1] == board[2][0]:
+        return board[0][2]
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board)== X or (winner(board)== O):
+        return True
+    elif actions(board) == None:
+        return True
+    else:
+        return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X:
+        return 1
+    elif winner(board) ==O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
